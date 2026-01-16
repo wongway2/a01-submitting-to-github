cc7dd3b39c19e2ee92beffc83d7ff786096a0833
Daniel Wong <dan2wong2@gmail.com>
2026-01-15 18:34:45 -0700

    Update a01_submitting_to_github.py

--- a01_submitting_to_github.py
+++ a01_submitting_to_github.py
@@ -1,11 +1,8 @@
-# Name: <Enter your name>
-# Description: <Enter a short description of your code>
-'''
-Write your code below these comments.
+# Name: Daniel Wong
+# Description: This is inputting a number and dividing by 2.
+#I like learning python.
 
-You can test your code using the testing tab on the left.
-When finished, upload your code by committing it and pushing it back up to 
-GitHub. I recommend using GitHub Desktop to do that.
-
-You can delete these comments after reading them.
-'''
+num = input("Please enter a whole number: ")
+num_int = int(num)
+new_number = num_int / 2
+print(f"{num_int} divided by 2 is {new_number}")
